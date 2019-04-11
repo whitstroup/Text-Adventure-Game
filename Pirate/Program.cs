7b900ea636be150e5c0f95d61e7722b8e07043fa
Whit <whit@stroups.net>
2019-04-11 10:55:35 -0500

    Added first adventure choices.

--- Pirate/Program.cs
+++ Pirate/Program.cs
@@ -6,7 +6,7 @@ namespace Pirate
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("ARRRR Welcome YEE my Pirate Adventure!");
+            Console.WriteLine("ARRRR Welcome YEE to my Pirate Adventure!");
             Console.WriteLine("");
             Console.WriteLine("Do ya have what it takes to be a pirate?!?!?");
             Console.WriteLine("Enter yes or no");
@@ -17,6 +17,22 @@ namespace Pirate
             if (answer == "yes")
             {
                 Console.WriteLine("I kneeeew yeee had it in ya!");
+                Console.WriteLine("Would ya like to go on a treasure adventure with me?");
+                Console.WriteLine("yes or no");
+                string answertwo = Console.ReadLine();
+
+                if (answertwo == "yes")
+                {
+                    Console.WriteLine("Your an adeventure lubber like I!");
+                    Console.WriteLine("AAARR I got two adventures on my horizon. Would you like to search for buried treasure?");
+                    Console.WriteLine("Or would yee rather go whale hunting?");
+                    Console.WriteLine("Enter 1 for buried treasure. Enter 2 for whale hunting.");
+                    int () = x;
+                    if (x = 1)
+                    {
+                        Console.WriteLine("TREASURE HUNTING!");
+                    }
+                }
             }
 
             else if (answer == "no")
@@ -27,6 +43,8 @@ namespace Pirate
             {
                 Console.WriteLine("You don't speak pirate do ya? Try again");
             }
+
+
 
         }
     }
